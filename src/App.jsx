c478b4dd7b98d67c46110cf8d699012060b36610
asnirmal2014 <asnirmal2014@gmail.com>
2025-10-15 12:12:39 +0530

    15th oct Taskform

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Progresstracker from "./Components/Progresstracker"
+import Taskform from "./Components/Taskform"
+import TaskList from "./Components/TaskList"
+
+export default function App() {
+  return(
+    <div>
+      <h1>Task Focus</h1>
+      <p>Our friendly TaskManager</p>
+      <Taskform />
+      <TaskList />
+      <Progresstracker />
+      <button>Clear all tasks</button>
+    </div>
+  )
+}
+

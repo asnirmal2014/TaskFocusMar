5aeb184373d7c2fd4e1e37323bb4b7e8c1570643
asnirmal2014 <asnirmal2014@gmail.com>
2025-10-16 11:10:07 +0530

    16th OCt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import Progresstracker from "./Components/Progresstracker"
-import Taskform from "./Components/Taskform"
-import TaskList from "./Components/TaskList"
+
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
   return(
     <div>
       <h1>Task Focus</h1>
       <p>Our friendly TaskManager</p>
-      <Taskform />
+      <Taskform addTask = {addTask}/>
       <TaskList />
       <Progresstracker />
       <button>Clear all tasks</button>
